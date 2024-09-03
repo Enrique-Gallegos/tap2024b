23218fedc69e75cddbfd56d5a212101abadf99df
Enrique Gallegos Linares <kikesssss12@gmail.com>
2024-09-03 17:31:47 -0600

    2/09/2024 Cambios

--- src/main/java/com/example/tap2024b/HelloApplication.java
+++ src/main/java/com/example/tap2024b/HelloApplication.java
@@ -4,37 +4,39 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import com.example.tap2024b.vistas.Calculadora;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
 
-    private Button btn1, btn2, btn3;
-
-    private VBox vbox;
-
-    private HBox hbox;
-
-    private BorderPane pane = new BorderPane();
+    private BorderPane bdpPrincipal;
+    private MenuBar mnbPrincipal;
+    private Menu menCompetencia1,menCompetencia2,menSalir;
+    private MenuItem mitCalculadora;
 
     public void CrearUI(){
-        btn1 = new Button("Boton 1");
-        btn2 = new Button("Boton 2");
-        btn3 = new Button("Boton 3");
-        vbox = new VBox(btn1, btn2, btn3);
-        hbox = new HBox(btn1, btn2, btn3);
-        pane.setBottom(hbox);
+        mitCalculadora = new MenuItem("Calculadora");
+        mitCalculadora.setOnAction(event -> new Calculadora());
+        menCompetencia1 = new Menu("Competencia 1");
+        menCompetencia1.getItems().addAll(mitCalculadora);
+        mnbPrincipal = new MenuBar(menCompetencia1);
+        bdpPrincipal = new BorderPane();
+        bdpPrincipal.setTop(mnbPrincipal);
     }
 
     @Override
     public void start(Stage stage) throws IOException {
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         CrearUI();
-        Scene scene = new Scene(pane, 320, 240);
+        Scene scene = new Scene(bdpPrincipal, 320, 240);
+        //scene.getStylesheets().add(getClass().getResource("/styles/main.css").toString());
+        scene.getStylesheets().add(getClass().getResource("/styles/main.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.setMaximized(true);
--- src/main/java/com/example/tap2024b/vistas/Calculadora.java
+++ src/main/java/com/example/tap2024b/vistas/Calculadora.java
@@ -1,5 +1,6 @@
 package com.example.tap2024b.vistas;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -7,27 +8,137 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class Calculadora extends Stage {
 
-    private Button[] arBtns;
+    private Button[][] arBtns;
     private TextField txtPantalla;
     private GridPane gdpTeclado;
-    private VBox vbox;
+    private VBox vBox;
     private Scene escena;
+    String[] strTeclas = {"7","8","9","*","4","5","6","/","1","2","3","-",".","0","=","+"};
+    private Button btnBorrar = new Button("<-");
+    private double x = 0, y = 0, res = 0;
+    private String operacion = "";
+    private boolean flag = false;
 
     private void CrearUI(){
-        arBtns = new Button[16];
+        arBtns = new Button[4][4];
         txtPantalla = new TextField("0");
+        txtPantalla.setAlignment(Pos.CENTER_RIGHT);
+        txtPantalla.setEditable(false);
         gdpTeclado = new GridPane();
-        vbox = new VBox();
-        escena = new Scene();
+        CrearTeclado();
+
+        btnBorrar.setOnAction(event -> borrarPantalla());
+        btnBorrar.setPrefSize(200,50);
+
+        vBox = new VBox(txtPantalla,gdpTeclado,btnBorrar);
+        vBox.setSpacing(1);
+        escena = new Scene(vBox,200,270);
     }
 
-    public Calculadora() {
+    private void CrearTeclado(){
+        for (int i = 0; i < arBtns.length; i++) {
+            for (int j = 0; j < arBtns[0].length; j++) {
+                arBtns[j][i] = new Button(strTeclas[4 * i + j]);
+                arBtns[j][i].setPrefSize(50, 50);
+                int finali = i, finalj = j;
+                arBtns[j][i].setOnAction(event -> detectarTecla(strTeclas[4 * finali + finalj]));
+                gdpTeclado.add(arBtns[j][i],j,i);
+            }
+        }
+    }
+
+    public Calculadora(){
+        CrearUI();
         this.setTitle("Calculadora");
-
-
-        this.setScene();
+        this.setScene(escena);
         this.show();
+    }
+
+    private void borrarPantalla(){
+        txtPantalla.clear();
+        txtPantalla.setText("0");
+
+    }
+
+    private void detectarTecla(String tecla) {
+        if (detectarOperacion(tecla)){
+            flag = true;
+        } else {
+            if (flag)
+                txtPantalla.clear();
+            flag = false;
+            if (txtPantalla.getText().equals("0"))
+                txtPantalla.setText("");
+            txtPantalla.appendText(tecla);
+        }
+    }
+
+    private boolean detectarOperacion(String tecla) {
+        switch(tecla){
+            case "+":
+                if (!flag)
+                    if (!operacion.equals(""))
+                        resultado();
+                operacion = "+";
+                x = Double.parseDouble(txtPantalla.getText());
+                return true;
+            case "-":
+                if (!flag)
+                    if (!operacion.equals(""))
+                        resultado();
+                operacion = "-";
+                x = Double.parseDouble(txtPantalla.getText());
+                return true;
+            case "*":
+                if (!flag)
+                    if (!operacion.equals(""))
+                        resultado();
+                operacion = "*";
+                x = Double.parseDouble(txtPantalla.getText());
+                return true;
+            case "/":
+                if (!flag)
+                    if (!operacion.equals(""))
+                        resultado();
+                operacion = "/";
+                x = Double.parseDouble(txtPantalla.getText());
+                return true;
+            case "=":
+                if(!flag)
+                    resultado();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void resultado(){
+        y = Double.parseDouble(txtPantalla.getText());
+        realizarOperacion();
+    }
+
+    private void realizarOperacion(){
+        if (!operacion.equals("")) {
+            switch (operacion) {
+                case "+":
+                    res = x + y;
+                    break;
+                case "-":
+                    res = x - y;
+                    break;
+                case "*":
+                    res = x * y;
+                    break;
+                case "/":
+                    res = x / y;
+                    break;
+            }
+            txtPantalla.setText("" + res);
+            operacion = "";
+        }
     }
 }
